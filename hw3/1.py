01bfa9f9262aa009b81f553539c0a0ead46fb041
elizaveta <liza_sheina@rambler.ru>
2023-04-03 00:46:19 +0300

    hw3

--- hw3/1.py
+++ hw3/1.py
@@ -1,9 +1,27 @@
-
 def can_eat(knight_pos, other_pos):
-    # Вычисляем разницу между координатами коня и другой фигуры
+    """
+    Функция can_eat проверяет, может ли конь съесть другую фигуру
+    на шахматной доске.
+
+    Аргументы:
+    - knight_pos: кортеж с координатами коня (x, y)
+    - other_pos: кортеж с координатами другой фигуры (x, y)
+
+    Возвращает: True, если конь может съесть другую фигуру, иначе False.
+    """
+    # Проверяем, что расстояние между x и y координатами коня и другой фигуры
+    # соответствует правилам хода коня на шахматной доске
     dx = abs(knight_pos[0] - other_pos[0])
     dy = abs(knight_pos[1] - other_pos[1])
-    # Проверяем, является ли разница достаточной для того, чтобы конь смог съесть фигуру
-    return (dx == 1 and dy == 2) or (dx == 2 and dy == 1)
-    result = can_eat((2, 1), (4, 2))
-    print(result) # Вывод: True
+    if dx == 1 and dy == 2 or dx == 2 and dy == 1:
+        return True
+    else:
+        return False
+
+
+
+result = can_eat((1, 2), (2, 4))
+print(result) # True
+
+result = can_eat((4, 4), (6, 5))
+print(result) # True
